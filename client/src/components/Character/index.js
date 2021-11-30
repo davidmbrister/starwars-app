8a6986314f32b3eb3a6101138545977b0ab0413f
davidmbrister <davidmurdockbrister@gmail.com>
2021-11-29 21:30:12 -0500

    search bar dynamically populates character card(onchange)

--- client/src/components/Character/index.js
+++ client/src/components/Character/index.js
@@ -1,24 +1,23 @@
-import React from 'react'
-import Fetch from '../../Fetch'
+import React from "react";
+import Fetch from "../../Fetch";
 
-const Character = ({characterId}) => {
+const Character = ({ characterId }) => {
   return (
     <div>
-      <Fetch 
-          uri={`http://localhost:9000/starWarsAPI/${characterId}`} 
-          renderSuccess={CharacterCard} />
+      {!characterId ? null : (
+        <Fetch
+          uri={`http://localhost:9000/starWarsAPI/${characterId}`}
+          renderSuccess={CharacterCard}
+        />
+      )}
     </div>
-  )
+  );
+};
+
+function CharacterCard(characterData) {
+  const character = characterData;
+  console.log("char data " + characterData);
+  return <div>{JSON.stringify(character)}</div>;
 }
 
-function CharacterCard (characterData) {
-  const character =  characterData
-  console.log("char data " + characterData)
-  return (
-    <div>
-      {JSON.stringify(character)}
-    </div>
-  )
-}
-
-export default Character
+export default Character;
